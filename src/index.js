f10fd83ab463ad0da37bd768ad7b02e75072294f
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-11-05 21:18:04 -0500

    Completed script and local .env

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 console.log('Hello world, this is a simple Node app')
-console.log(`Your name is ${process.env.name}.`)
+console.log(`Your name is ${process.env.name}. Your favorite color is ${process.env.favColor}. Your favorite food is ${process.env.favFood}.`)
